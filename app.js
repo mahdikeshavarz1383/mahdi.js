7187b89e2a45d4681c2cb2b7ade6ab6a23be8d92
mahdikeshavarz1383 <mahdikeshavarz1383m@gmail.com>
2026-02-27 10:03:18 -0800

    10th commit

--- app.js
+++ app.js
@@ -1,149 +1,35 @@
-console.time("mahdi");
+
+//...................................................................
 import express from "express";
 import axios from "axios";
-import dotenv from 'dotenv';
-import os from "os";
- import fs from "fs";
-dotenv.config()
-import promptSync from 'prompt-sync';
- const prompt = promptSync({ sigint: true });
-console.log(os.freemem());
-import { json } from 'stream/consumers';
-
-//const port=process.env.PORT || 3000;
-const port=3000;
 const app = express();
-//const API_KEY="6351130fc60eeb5af150f6ea411eba42";
-fs.readFile("mahdi.txt","utf-8", (err, data)=> {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(data);
-    }
-})
- const user={
-     username:"mahdi keshavarz",
-     password:"12345",
-     age:23,
- }
-const raw=JSON.stringify(user,null,2);
-fs.writeFile("data.json",raw, (err,data)=>{
-    if (err) {console.log(err); return;}
-    else{console.log(data);}
-})
+const port=3000;
 
- app.get("/", (req, res)=>{
-    res.send(raw);
- })
-  app.listen(port, ()=>{
-  console.log(`the program is runnig on port:${port}`)   
-  })
-fs.writeFile("data.txt",raw,(err)=>{
-    if (err) {console.log(err);}
-    else {console.log("saved");}
-})
-console.log("this is new version");
+const API_KEY = "6351130fc60eeb5af150f6ea411eba42";
 
+ app.get("/weather/:city", async (req, res) => {
+   const city = req.params.city;
 
+  try {
+     const url =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=fa`
 
-//------------------------------------------------------------
+    const response = await axios.get(url);
 
-const userOfdData=[]
-while (true) {
-    const inputName=prompt("enter your name");
-    if (inputName) {
-        if (inputName==="exit") {break;}
-        else{
-            const inputPhone=prompt("enter your phone");
-           
-            userOfdData.push(inputName);
-            userOfdData.push(inputPhone);
-        }
-    }
+    const data = response.data;
 
-}
-const output=JSON.stringify(userOfdData,null,2)
+    res.json({
+       city: data.name,
+       temp: data.main.temp,
+       feels_like: data.main.feels_like,
+       humidity: data.main.humidity,
+       description: data.weather[0].description,
+       wind: data.wind.speed
+     });
 
-fs.writeFile("user.json",output, (err,data)=>{
-    if (err) {console.log(err);}
-    else{console.log(data);}
-})
+  } catch (err) {
+     res.json({ error: "شهر پیدا نشد یا مشکلی در API وجود دارد" });
+   }
+ });
 
-fs.writeFile("user.txt",output,(err)=>{
-    if(err) {console.log(`this is:${err}`)}
-})
-
-//---------------------------------------------------------
-// app.get("/", (req,res)=>{
-//     res.send(output);
-// })
-// app.listen(port, ()=>{
-//     console.log(`the program is running on port :${port}`);
-// })
-//.............................
-
-// const customerOfjson=JSON.stringify(customer,null,2)
-// console.log(customerOfjson);
-// app.get("/", (req, res)=>{
-//     res.send(customerOfjson);
-// })
-// app.listen(port, ()=>{
-//     console.log(`the prohram is running on port`);;
-// })
-
-//-------------------------------------------------------------------------
-
-
-
-
-// const API_KEY = "6351130fc60eeb5af150f6ea411eba42";
-
-// app.get("/weather/:city", async (req, res) => {
-//   const city = req.params.city;
-
-//   try {
-//     const url =`http://api.openweathermap.org/data/2.5/weather? q=${city}&appid=${API_KEY}&units=metric&lang=fa`
-
-//     const response = await axios.get(url);
-
-//     const data = response.data;
-
-//     res.json({
-//       city: data.name,
-//       temp: data.main.temp,
-//       feels_like: data.main.feels_like,
-//       humidity: data.main.humidity,
-//       description: data.weather[0].description,
-//       wind: data.wind.speed
-//     });
-
-//   } catch (err) {
-//     res.json({ error: "شهر پیدا نشد یا مشکلی در API وجود دارد" });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Weather API is running on http://localhost:${port})`);})
-
-//-----------------------------------------------------------------------------------
-
-// import TelegramBot from "node-telegram-bot-api";
-
-// const token = "8638909410:AAG8M8Q2pKf7MFxM9941MJe1_GDUZumQjuc";
-
-// // فعال کردن Long Polling
-// const bot = new TelegramBot(token, { polling: true });
-
-// // وقتی کاربر /start بزند
-// bot.onText(/\/start/, (msg) => {
-//   const chatId = msg.chat.id;
-//   bot.sendMessage(chatId, "سلام");
-// });
-
-//-----------------------------------------------
-
-
-
-
-console.timeEnd("mahdi");
+ app.listen(port, () => {
+  console.log(`Weather API is running on http://localhost:${port})`);})
